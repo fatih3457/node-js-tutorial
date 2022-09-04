538e04099c6ff7c4dca4de69c199a5b8785403b1
Fatih Özcan <58815565+fatih3457@users.noreply.github.com>
2022-09-04 23:01:41 +0300

    first commit

---
+++ my-module.js
@@ -0,0 +1,31 @@
+export default (name) => {
+    
+    console.log(`hello ${name}`)
+};
+
+ const topla = (a, b) => a + b;
+ const cikar = (a, b) => a - b;
+
+ const text = 'Text';
+ const user = {
+    name: "Fatih",
+    surname: "Özcan",
+};
+
+ const users = [
+{
+    name: "Ahmet",
+    surname: "Tarık",
+},
+{
+    name: "Tayfun",
+    surname: "Erbilen",
+},
+];
+
+export { topla, cikar, text, user, users};
+
+
+
+
+
